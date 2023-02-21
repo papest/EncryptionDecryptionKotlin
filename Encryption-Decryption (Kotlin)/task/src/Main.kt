edc899420ea44423ed55dcc534e2f69edc4399d9
Epap <peia@inbox.ru>
2023-02-21 10:42:18 +0300

    Now offset-encrypt all characters

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,6 +1,5 @@
 package encryptdecrypt
 
-const val ALPHABET_LENGTH = 26
 fun main() {
     println(shiftEncrypt(readln(), readln().toInt()))
 }
@@ -8,12 +7,8 @@ fun main() {
 fun shiftEncrypt(input: String, key: Int): String {
     val result = mutableListOf<Char>()
     for (ch in input) {
-        if (ch in 'a'..'z') {
-            result.add('a' + (ch - 'a' + key) % ALPHABET_LENGTH)
-        } else {
-            result.add(ch)
+        result.add(ch + key)
 
-        }
     }
     return result.joinToString("")
 
