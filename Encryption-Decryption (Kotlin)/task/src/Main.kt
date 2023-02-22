a7fbe8fe0a84a4c4e6f94ee3205771258c986ab3
Epap <peia@inbox.ru>
2023-02-22 23:57:03 +0300

    Now the program works with command line arguments

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,18 +1,38 @@
 package encryptdecrypt
 
-fun main() {
-    val choice = readln()
-    val input = readln()
-    val key = readln().toInt()
+fun main(args: Array<String>) {
+    val parameters = Args(args)
 
     println(
-        if (choice == "enc") {
-            shiftEncrypt(input, key)
+        if (parameters.mode == "enc") {
+            shiftEncrypt(parameters.data, parameters.key)
         } else {
-            shiftDecrypt(input, key)
+            shiftDecrypt(parameters.data, parameters.key)
         }
     )
+
+
 }
+
+class Args(args: Array<String>) {
+    var mode: String = "enc"
+    var key: Int = 0
+    var data: String = ""
+
+    init {
+        for (i in 0..2) {
+
+            when (args[i + i]) {
+                "-mode" -> mode = args[i + i + 1]
+                "-key" -> key = args[i + i + 1].toInt()
+                "-data" -> data = args[i + i + 1]
+            }
+        }
+    }
+
+
+}
+
 
 fun shiftEncrypt(input: String, key: Int): String {
     val result = mutableListOf<Char>()
